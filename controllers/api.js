fe9f265f383cb05ea4e2e58861643fb5decdc17a
JC Renner <jc-renner@lu-tec.com>
2021-11-17 09:55:43 -0500

    deployment

--- controllers/api.js
+++ controllers/api.js
@@ -2,9 +2,28 @@ const router = require('express').Router();
 const Workout = require('../models/workout');
 
 router.get("/workouts", (req, res) => {
-    Workout.find({})
-    .sort({ date: -1 })
+    // Workout.find({})
+    // .sort({ date: -1 })
+    // .then(dbWorkout => {
+    //   res.json(dbWorkout);
+    // })
+    // .catch(err => {
+    //   res.status(400).json(err);
+    // });
+    Workout.aggregate(
+      [
+        {
+          // $addfields - need total duration
+          $addFields: {
+            totalDuration: {
+              $sum: '$exercises.duration'
+            }
+          }
+        },
+      ]
+    )
     .then(dbWorkout => {
+      console.log(dbWorkout);
       res.json(dbWorkout);
     })
     .catch(err => {
